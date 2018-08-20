f4b7c2c445e88bdb6f8b04cfe4c4c8b051219902
Peter Jurkovic <petojurkovic@gmail.com>
2018-08-20 23:53:03 +0100

    Possible solution

--- src/main/java/com/nexmo/interview/book/SorterBySection.java
+++ src/main/java/com/nexmo/interview/book/SorterBySection.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
-public class SorterBySection implements Sortable {
+public class SorterBySection implements Sortable, Comparator<BookSection> {
 
 
 
@@ -18,29 +18,29 @@ public class SorterBySection implements Sortable {
      * Example
      *
      * <code>
-     * 	Input output
+     *  Input output
      *
-     * 	1.1   Sub title - H2
-     * 	1.2   Sub title - H2
-     * 	2     title - H1
-     * 	1.2.1 Sub Sub title - H3
-     * 	1.3   Sub title - H2
-     * 	2.1   Sub title - H2
-     * 	1     Title - H1
-     * 	2.10  Sub title - H2
-     * 	1.10  Sub title - H2
+     *  1.1   Sub title - H2
+     *  1.2   Sub title - H2
+     *  2     title - H1
+     *  1.2.1 Sub Sub title - H3
+     *  1.3   Sub title - H2
+     *  2.1   Sub title - H2
+     *  1     Title - H1
+     *  2.10  Sub title - H2
+     *  1.10  Sub title - H2
      *
-     * 	Expected output
+     *  Expected output
      *
-     * 	1     Title - H1
-     * 	1.1   Sub title - H2
-     * 	1.10  Sub title - H2
-     * 	1.2   Sub title - H2
-     * 	1.2.1 Sub Sub title - H3
-     * 	1.3   Sub title - H2
-     * 	2     title - H1
-     * 	2.1   Sub title - H2
-     * 	2.10  Sub title - H2
+     *  1     Title - H1
+     *  1.1   Sub title - H2
+     *  1.10  Sub title - H2
+     *  1.2   Sub title - H2
+     *  1.2.1 Sub Sub title - H3
+     *  1.3   Sub title - H2
+     *  2     title - H1
+     *  2.1   Sub title - H2
+     *  2.10  Sub title - H2
      </code>
      *
      * - Max book section depth is 3
@@ -51,9 +51,49 @@ public class SorterBySection implements Sortable {
      */
     @Override
     public List<BookSection> sort(List<BookSection> list) {
-        // TODO implement
-        return list;
+        // TODO implement me
+        return list.stream()
+                .sorted(this)
+                .collect(toList());
+
     }
 
+    @Override
+    public int compare(BookSection bs1, BookSection bs2) {
+        CompareableSection section1 = new CompareableSection( bs1.getSection() );
+        CompareableSection section2 = new CompareableSection( bs2.getSection() );
+        return section1.compareTo(section2);
+    }
+
+
+
+    static class CompareableSection implements Comparable<CompareableSection>{
+
+        private final int[] parts;
+
+        CompareableSection(String section) {
+            this.parts = Arrays.stream(section.split("\\."))
+                    .mapToInt(Integer::valueOf)
+                    .toArray();
+        }
+
+        @Override
+        public int compareTo(CompareableSection o) {
+            int minDepth = Math.min(o.parts.length, this.parts.length);
+
+            for(int depth = 0; depth < minDepth; depth++){
+
+                if (this.parts[depth] != o.parts[depth])
+                    return  this.parts[depth] < o.parts[depth]? -1 : 1;
+
+            }
+            return this.parts.length - o.parts.length;
+        }
+
+        @Override
+        public String toString() {
+            return Arrays.toString(parts);
+        }
+    }
 
 }
